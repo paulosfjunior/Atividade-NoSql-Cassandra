b05e701c6a8a68973b4a483fa7bebbd2560276bb
Paulo Freitas <paulosfjunior@gmail.com>
2019-04-24 15:45:16 -0300

    Configurações basicas do app.js, instalado packges e colocado ico e png na pasta images

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const bodyParser = require('body-parser')
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const serveStatic = require('serve-static')
+const serveFavicon = require('serve-favicon')
 
-var indexRouter = require('./routes/index');
+var indexRouter = require('./routes/index')
 var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express()
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
+app.use(serveStatic(path.join(__dirname, 'public')))
+app.use(serveFavicon(path.join(__dirname, '/public/images/favicon.ico')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
 
-module.exports = app;
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+  // render the error page
+  res.status(err.status || 500)
+  res.render('error')
+})
+
+module.exports = app
