5c0edbffa72aa67483f00c41f570a0ca5d32ba69
Paulo Freitas <paulosfjunior@gmail.com>
2019-04-30 13:45:45 -0300

    Arquivos de servidor movidos para pasta server

--- server/services/usuario.service.js
+++ server/services/usuario.service.js
@@ -24,12 +24,28 @@ function keyspaceExiste() {
   });
 }
 
-function tabelaExiste() {
+function tipoExiste() {
   return new Bluebird((resolve, reject) => {
     keyspaceExiste()
       .then((r) => {
+        const query = "CREATE TYPE IF NOT EXISTS atividadenosql.endereco " + 
+                      "(endereco text, bairro text, cidade text, pais text, cep text)";
+    
+        cassandra.execute(query, (e, r) => {
+          if (e) reject(e);
+          if (r) resolve(r);
+        });
+      })
+      .catch((e) => reject(e));
+  });
+}
+
+function tabelaExiste() {
+  return new Bluebird((resolve, reject) => {
+    tipoExiste()
+      .then((r) => {
         const query = "CREATE TABLE IF NOT EXISTS atividadenosql.usuario " + 
-                      "(id timeuuid, usuario text, nome text, endereco text, email text, hash text, salt text, PRIMARY KEY(id))";
+                      "(id timeuuid, usuario text, nome text, endereco frozen<endereco>, email text, hash text, salt text, PRIMARY KEY(id))";
     
         cassandra.execute(query, (e, r) => {
           if (e) reject(e);
@@ -44,10 +60,19 @@ function criarRegistro(p) {
   return new Bluebird((resolve, reject) => {
     tabelaExiste()
       .then((r) => {
+        let endereco = {
+          endereco: p.endereco,
+          bairro: p.bairro,
+          cidade: p.cidade,
+          pais: p.pais,
+          cep: p.cep
+        }
+
+        console.log({p,endereco})
         const query = "INSERT INTO atividadenosql.usuario " +
                       "(id, usuario, nome, endereco, email, hash, salt) VALUES " +
-                      "(now(), '" + p.usuario + "', '" + p.nome + "', '" + p.endereco + "', '" + p.email + "', '" + p.hash + "', '" + p.salt + "')";
-    
+                      "(now(), '" + p.usuario + "', '" + p.nome + "', " + endereco + ", '" + p.email + "', '" + p.hash + "', '" + p.salt + "')";
+        console.log({query})
         cassandra.execute(query, (e, r) => {
           if (e) reject(e);
           if (r) resolve(r);
@@ -65,7 +90,7 @@ function listarRegistro() {
     
         cassandra.execute(query, (e, r) => {
           if (e) reject(e);
-          if (r) resolve(r);
+          if (r) resolve(r.rows);
         });
       })
       .catch((e) => reject(e));
@@ -121,7 +146,7 @@ function procurarRegistro(p) {
 ;
         cassandra.execute(query, (e, r) => {
           if (e) reject(e);
-          if (r) resolve(r);
+          if (r) resolve(r.rows);
         });
       })
       .catch((e) => reject(e));
